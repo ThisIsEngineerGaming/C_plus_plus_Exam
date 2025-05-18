8fece572042072a960518b3b62d97a96030cab08
EngineerGamer <115916718+ThisIsEngineerGaming@users.noreply.github.com>
2025-05-18 12:55:28 +0300

    Add files via upload

---
+++ Phonebook/Phonebook/Phonebook.cpp
@@ -0,0 +1,36 @@
+
+#include "header.h"
+
+int main() {
+    int choice;
+    bool running = true;
+    loadFromFile();
+    while (running) {
+        cout << "\n--- PHONE BOOK MENU ---\n";
+        cout << "1. Add Contact\n";
+        cout << "2. View Contacts\n";
+        cout << "3. Search by Name\n";
+        cout << "4. Delete Contact\n";
+        cout << "5. Edit Contact\n";
+        cout << "6. Exit+Save\n";
+        cout << "Choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice) {
+        case 1: addContact(); break;
+        case 2: viewContacts(); break;
+        case 3: searchContact(); break;
+        case 4: deleteContact(); break;
+        case 5: editContact(); break;
+        case 6:
+            saveToFile(); 
+            cout << "Goodbye!\n";
+            running = false;
+            break;
+        default:
+            cout << "Invalid choice.\n";
+            break;
+        }
+    }
+}
